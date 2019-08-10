038ed1d313c759e33caab7a86d2f9da9cad213e2
Nima Hejazi <nima@Nimas-MacBook-Pro.local>
2019-08-10 18:06:34 +0430

    Add linux support

--- isvim.c
+++ isvim.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <libproc.h>
 #include "isvim.h"
 #include <stdlib.h>
 #include <string.h>
+#ifdef __APPLE__
+  #include <libproc.h>
+#elif __linux__
+  #include <sys/types.h>
+  #include <dirent.h>
+  #include <ctype.h>
+#endif
 
+#ifdef __APPLE__
 int pid_info(pid_t pid, proc_t *rproc)
 {
   struct proc_bsdinfo proc;
@@ -19,6 +26,44 @@ int pid_info(pid_t pid, proc_t *rproc)
   strncpy(rproc->name, proc.pbi_name, sizeof(rproc->name));
   return ret;
 }
+#elif __linux__
+DIR *dir;
+
+// if 0 returned, there is an error
+int pid_info(pid_t pid, proc_t *rproc)
+{
+  if (pid == 0)
+    return 0;
+  FILE *fd;
+  char filepath[32];
+  char buffer[512];
+  snprintf(filepath, sizeof(filepath), "/proc/%d/status", pid);
+  if ((fd = fopen(filepath, "r")) == NULL)
+  {
+    fprintf(stderr, "Error opening %s file\n", filepath);
+    return 0;
+  }
+
+  while(fgets(buffer, sizeof(buffer), fd))
+  {
+    char *title = strtok(buffer, ":\t");
+    char *value = strtok(NULL, ":\t\n");
+    if (!strcmp(title, "Name"))
+      strncpy(rproc->name, value, sizeof(rproc->name));
+    else if (!strcmp(title, "Pid"))
+      rproc->pid = atol(value);
+    else if (!strcmp(title, "PPid"))
+      rproc->ppid = atol(value);
+
+
+    // if name if not filled, 0 will be returned return sizeof(rproc->name);
+  }
+
+  fclose(fd);
+  return sizeof(rproc->name);
+  
+}
+#endif
 
 int main()
 {
@@ -27,7 +72,9 @@ int main()
   proc_t cur_proc, par_proc;
 
   pid_info(ppid, &cur_proc);
+
   // when return 0, it means shell has no parent
+  
   if (!pid_info(cur_proc.ppid, &par_proc))
     return 1;
 
